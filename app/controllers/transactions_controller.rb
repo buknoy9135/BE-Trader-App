ad9e949f79eb9fd2c5c7758fbfe30272a9813c5f
russ <cacdacdave@yahoo.com>
2025-07-18 20:09:39 +0800

    added new, create, index on transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,4 +4,32 @@ class TransactionsController < ApplicationController
     def new
     @transaction = Transaction.new
     end
+
+    def create
+    @transaction = current_user.transactions.build(transaction_params)
+    @transaction.executed_at = Time.current
+    @transaction.status = "executed"
+
+    if @transaction.save
+      redirect_to transactions_path, notice: "Transaction successful"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @transactions = current_user.transactions.order(executed_at: :desc)
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:asset_symbol, :transaction_type, :quantity, :price)
+  end
+
+  def ensure_approved_user
+    if current_user.trader? && !current_user.approved?
+      redirect_to root_path, alert: "Your account is not approved yet."
+    end
+  end
 end
