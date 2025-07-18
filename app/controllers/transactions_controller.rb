6e8891157ac710b7970890b60785d75d90c49064
russ <cacdacdave@yahoo.com>
2025-07-19 01:08:47 +0800

    modified transactions_controller to work with api

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,36 +1,54 @@
 class TransactionsController < ApplicationController
-    before_action :authenticate_user!
+	before_action :authenticate_user!
 
-    def new
-    @transaction = Transaction.new
-    end
+	def new
+		@transaction = Transaction.new
+	end
 
-    def create
-    price = StockPriceService.latest_price(params[:transaction][:asset_symbol])
-    @transaction = current_user.transactions.build(transaction_params)
-    @transaction.executed_at = Time.current
-    @transaction.status = "executed"
+	def create
+		@transaction = current_user.transactions.new(transaction_params)
+		@transaction.transaction_type = "buy"
+		@transaction.status = "executed"
+		@transaction.executed_at = Time.current
 
-    if @transaction.save
-      redirect_to transactions_path, notice: "Transaction successful"
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+		price = StockPriceService.latest_price(@transaction.asset_symbol)
+			if price.nil?
+				flash[:alert] = "Unable to fetch stock price."
+				render :new and return
+			end
 
-  def index
-    @transactions = current_user.transactions.order(executed_at: :desc)
-  end
+		@transaction.price = price
+		@transaction.total_amount = price * @transaction.quantity
 
-  private
+			if current_user.balance < @transaction.total_amount
+				flash[:alert] = "Insufficient funds."
+				render :new and return
+			end
 
-  def transaction_params
-    params.require(:transaction).permit(:asset_symbol, :transaction_type, :quantity, :price)
-  end
+		ActiveRecord::Base.transaction do
+			current_user.update!(balance: current_user.balance - @transaction.total_amount)
+			@transaction.save!
+		end
+			flash[:notice] = "Stock bought successfully!"
+			redirect_to transactions_path
+		rescue => e
+			flash[:alert] = "Transaction failed: #{e.message}"
+			render :new
+	end
 
-  def ensure_approved_user
-    if current_user.trader? && !current_user.approved?
-      redirect_to root_path, alert: "Your account is not approved yet."
-    end
-  end
+	def index
+		@transactions = current_user.transactions.order(executed_at: :desc)
+	end
+
+	private
+
+	def transaction_params
+		params.require(:transaction).permit(:asset_symbol, :transaction_type, :quantity, :price)
+	end
+
+	def ensure_approved_user
+		if current_user.trader? && !current_user.approved?
+		redirect_to root_path, alert: "Your account is not approved yet."
+		end
+	end
 end
